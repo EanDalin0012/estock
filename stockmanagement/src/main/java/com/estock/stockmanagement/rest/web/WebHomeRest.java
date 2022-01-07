3790c31eb17c7a8720527883ec4c61377bbbbb8f
eandalined@gmail.com <johndoe@example.com>
2022-01-08 00:29:50 +0700

    commit

--- stockmanagement/src/main/java/com/estock/stockmanagement/rest/web/WebHomeRest.java
+++ stockmanagement/src/main/java/com/estock/stockmanagement/rest/web/WebHomeRest.java
@@ -4,12 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping(value = "/api/web")
-@Slf4j
 public class WebHomeRest {
-
+	private static final Logger log = LoggerFactory.getLogger(WebHomeRest.class);
 	@GetMapping(value = "/home")
 	public String index() {
 		log.info("Web Home");
