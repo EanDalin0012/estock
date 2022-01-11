1d873fe3ce690b9fdca536a22a65e325650e8885
eandalined@gmail.com <johndoe@example.com>
2022-01-11 21:44:01 +0700

    commit

--- stockmanagement/src/main/java/com/estock/stockmanagement/provider/product/data/adapter/ProductAdapter.java
+++ stockmanagement/src/main/java/com/estock/stockmanagement/provider/product/data/adapter/ProductAdapter.java
@@ -1,12 +1,15 @@
 package com.estock.stockmanagement.provider.product.data.adapter;
 
 import com.estock.stockmanagement.common.adapter.CommonAdapter;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(callSuper=true)
--- stockmanagement/src/main/java/com/estock/stockmanagement/rest/web/WebHomeRest.java
+++ stockmanagement/src/main/java/com/estock/stockmanagement/rest/web/WebHomeRest.java
@@ -3,7 +3,6 @@ package com.estock.stockmanagement.rest.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
