4e8df679a6e0c84c36fd3e3f74fe8272b7bde6bf
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2022-01-30 11:13:04 +0700

    back-up database

---
+++ api/src/main/java/com/estock/api/dto/UserAuthorityDTO.java
@@ -0,0 +1,14 @@
+package com.estock.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserAuthorityDTO {
+    private int id;
+}
