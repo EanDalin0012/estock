726029df22ae852d340c2b87b4b931804b1894d0
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2022-02-12 00:19:22 +0700

    commit

--- estock/src/app/e-sale/e-sale-routing.module.ts
+++ estock/src/app/e-sale/e-sale-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ESaleComponent } from './e-sale.component';
+import { SaleInvoiceComponent } from './sale-invoice/sale-invoice.component';
+import { SaleComponent } from './sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ESaleComponent,
+    children: [
+      {path: '', component: SaleComponent},
+      {path: 'sale-invoice', component: SaleInvoiceComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- estock/src/app/e-sale/sale/sale.component.ts
+++ estock/src/app/e-sale/sale/sale.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/e-share/service/data.service';
+import { HTTPService } from 'src/app/e-share/service/http.service';
+import {AllCommunityModules} from "@ag-grid-community/all-modules";
+import {ColDef} from "ag-grid-community";
+import { TemplateAPI } from 'src/app/e-share/constants/common.api';
+import { UserInfo } from 'src/app/e-share/data/user-inf';
+import { EmployeeRequest } from 'src/app/e-share/data/employee-request';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sale',
   templateUrl: './sale.component.html',
@@ -7,9 +15,141 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaleComponent implements OnInit {
 
-  constructor() { }
+  pagination = true;
+  paginationPageSize = 20;
+  gridApi:any;
+  gridColumnApi :any;
+  public modules: any[] = AllCommunityModules;
+  frameworkComponents: any;
+  defaultColDef: any;
+  columnDefs: ColDef[] = [];
+  rowData: any;
+  rowSelection: any;
+  isRowSelectable: any;
+
+  pinnedTopRowData: any;
+  pinnedBottomRowData: any;
+  employeeRequests: EmployeeRequest[] = [];
+
+  constructor(
+    private hTTPService: HTTPService,
+    private dataService: DataService,
+    private titleService: Title,
+    private router: Router,
+  ) {
+    const url = (window.location.href).split('/');
+    console.log(url)
+    this.dataService.visitParamRouterChange(url[3]);
+    this.titleService.setTitle('Employee Request');
+  }
 
   ngOnInit(): void {
+    this.inquiry();
+    this.columnDefs = [
+      {
+        headerName: '#',
+        field: 'name', minWidth: 50, width: 50
+      },
+      {
+        headerName: 'Product',
+        field: 'desc'
+      },
+      {
+        headerName: 'Sale Type Product',
+        field: 'desc'
+      },
+      {
+        headerName: 'Qty',
+        field: 'desc'
+      },
+      {
+        headerName: 'Unit Price',
+        field: 'desc'
+      },
+      {
+        headerName: 'Total',
+        field: 'desc'
+      }
+    ];
+
+    this.defaultColDef = {
+      editable: false,
+      sortable: true,
+      flex: 1,
+      // minWidth: 50,
+      filter: true,
+      resizable: true,
+    };
+
+    this.rowSelection = 'multiple';
+    this.isRowSelectable = function (rowNode: any) {
+      return rowNode.data;
+    };
+  }
+
+  onSelectionChanged(event: any) {
+    const selectedRows = this.gridApi.getSelectedRows();
+  }
+
+
+  onCellValueChanged(event:any) {
+    console.log('data after changes is: ', event.data);
+    const data = event.data as UserInfo;
+    console.log(data);
+    if(data) {
+      this.hTTPService.Post(TemplateAPI.USER_INFO.UPDATE, data).then(response => {
+        console.log(response);
+        this.inquiry();
+      });
+    }
+  }
+
+  onCellDoubleClicked(event:any) {
+    if(event) {
+      // const jsonString = JSON.stringify(this.lstUser[event.rowIndex]);
+      // const encryptString = EncryptionUtil.encrypt(jsonString.toString()).toString();
+      // Utils.setSecureStorage(LOCAL_STORAGE.UserDetail, encryptString);
+      // this.router.navigate(['account/user-detail']);
+    }
+  }
+
+  onGridReady(params:any) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    // this.inquiry();
+  }
+
+  inquiry() {
+    for (let index = 0; index < 100; index++) {
+      this.employeeRequests.push({
+        id: 'OT-2022-0000'+index,
+        requester: 'Ean Dalin '+index,
+        position: 'API Developer',
+        department: 'Technology',
+        purspose: '?',
+        status: 'Approved'
+      });
+    }
+    this.rowData = [
+      {
+        name: 'Customser',
+        desc: 'Sale for Customer'
+      },
+      {
+        name: 'Member',
+        desc: 'Sale for Member'
+      }
+    ]
+
+    const api = '/api/user-info';
+    // this.hTTPService.Get(TemplateAPI.USER_INFO.URL).then(response =>{
+    //   console.log("response", response)
+    //   this.rowData = response;
+    // });
+  }
+
+  onNewRequest() {
+    this.router.navigate(['/employee-request/form']);
   }
 
 }
