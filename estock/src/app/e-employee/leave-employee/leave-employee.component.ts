64a1e728b4f542d4f4d0116514a6d7679aef4ec2
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2022-02-11 12:44:48 +0700

    add font awsome free

--- estock/src/app/e-employee/leave-employee/leave-employee.component.ts
+++ estock/src/app/e-employee/leave-employee/leave-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-leave-employee',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-employee.component.css']
 })
 export class LeaveEmployeeComponent implements OnInit {
+  isSubmitted = false;
+  constructor(public fb: FormBuilder) { }
 
-  constructor() { }
+  /*########### Form ###########*/
+  registrationForm = this.fb.group({
+    gender: ['male', [Validators.required]]
+  })
 
   ngOnInit(): void {
+  }
+
+  // Submit Registration Form
+  onSubmit() {
+    this.isSubmitted = true;
+    if(!this.registrationForm.valid) {
+      return false;
+    } else {
+      alert(JSON.stringify(this.registrationForm.value))
+    }
+    return true;
   }
 
 }
