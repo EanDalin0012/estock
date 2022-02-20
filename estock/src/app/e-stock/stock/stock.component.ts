5c5e5bbec3fa78805aec8f303971086a14c2b4d0
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2022-02-20 19:31:16 +0700

    commit

--- estock/src/app/e-stock/stock/stock.component.ts
+++ estock/src/app/e-stock/stock/stock.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {
+  CheckboxSelectionCallbackParams,
+  ColDef,
+  GridReadyEvent,
+  HeaderCheckboxSelectionCallbackParams,
+} from 'ag-grid-community';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 @Component({
   selector: 'app-stock',
@@ -6,10 +15,136 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock.component.css']
 })
 export class StockComponent implements OnInit {
+  public columnDefs: ColDef[] = [
+    {
+      headerName: 'Date',
+      field: 'date',
+      minWidth: 170,
+      checkboxSelection: checkboxSelection,
+      headerCheckboxSelection: headerCheckboxSelection,
+    },
+    {
+      headerName: 'Warehouse',
+      field: 'warehouse'
+    },
+    {
+      headerName: 'Name',
+      field: 'name'
+    },
+    {
+      headerName: 'Unit',
+      field: 'unit'
+    },
+    {
+      headerName: 'Opening Stock',
+      field: 'openingStock'
+    },
+    {
+      headerName: 'Received',
+      field: 'received'
+     },
+    {
+      headerName: 'Total',
+      field: 'total'
+    },
+    {
+      headerName: 'Sales',
+      field: 'sales'
+    },
+    {
+      headerName: 'Closing Stock',
+      field: 'closingStock'
+    }
+  ];
+  public autoGroupColumnDef: ColDef = {
+    headerName: 'Group',
+    minWidth: 170,
+    field: 'athlete',
+    valueGetter: function (params) {
+      if (params.node!.group) {
+        return params.node!.key;
+      } else {
+        return params.data[params.colDef.field!];
+      }
+    },
+    headerCheckboxSelection: true,
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true,
+    },
+  };
+  public defaultColDef: ColDef = {
+    // editable: true,
+    // enableRowGroup: true,
+    // enablePivot: true,
+    // enableValue: true,
+    sortable: true,
+    resizable: true,
+    filter: true,
+    flex: 1,
+    minWidth: 200,
+  };
+  public rowSelection = 'multiple';
+  public rowGroupPanelShow = 'always';
+  public pivotPanelShow = 'always';
+  public rowData!: any[];
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit(): void {
   }
 
+  onGridReady(params: GridReadyEvent) {
+    // this.http
+    //   .get<any[]>('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    //   .subscribe((data) => (this.rowData = data));
+    this.rowData = stockDatas;
+  }
 }
+
+var checkboxSelection = function (params: CheckboxSelectionCallbackParams) {
+  // we put checkbox on the name if we are not doing grouping
+  return params.columnApi.getRowGroupColumns().length === 0;
+};
+var headerCheckboxSelection = function (
+  params: HeaderCheckboxSelectionCallbackParams
+) {
+  // we put checkbox on the name if we are not doing grouping
+  return params.columnApi.getRowGroupColumns().length === 0;
+};
+export interface Data {
+  date: string;
+  warehouse: string;
+  name: string;
+  unit: string;
+  openingStock: number;
+  received: number;
+  total: number;
+  sales: number;
+  closingStock: number;
+}
+
+export const stockDatas: Data[] = [
+  {
+    date: '01 Jan 2021',
+    warehouse: 'Warehouse 1',
+    name: 'China Apple',
+    unit: 'Kilogram (kg)',
+    openingStock: 364,
+    received: 450,
+    total: 658,
+    sales: 60,
+    closingStock: 80,
+  },
+  {
+    date: '02 Jan 2021',
+    warehouse: 'Warehouse 2',
+    name: 'Orange',
+    unit: 'Kilogram (kg)',
+    openingStock: 364,
+    received: 450,
+    total: 658,
+    sales: 60,
+    closingStock: 80,
+  }
+];
+
--- estock/src/app/e-warehouse/warehouse/warehouse.component.ts
+++ estock/src/app/e-warehouse/warehouse/warehouse.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AllCommunityModules } from '@ag-grid-community/all-modules';
+import { ColDef } from 'ag-grid-community';
+import {SrcComponent} from "../../e-share/component/src/src.component";
+import jsPDF from 'jspdf';
 @Component({
   selector: 'app-warehouse',
   templateUrl: './warehouse.component.html',
@@ -7,9 +10,169 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WarehouseComponent implements OnInit {
 
+  pagination = true;
+  paginationPageSize = 20;
+  gridApi:any;
+  gridColumnApi :any;
+  public modules: any[] = AllCommunityModules;
+  frameworkComponents: any;
+  defaultColDef: any;
+  columnDefs: ColDef[] = [];
+  rowData: any;
+  rowSelection: any;
+  isRowSelectable: any;
+
+
+  @ViewChild('content')
+  content!: ElementRef;
+  name = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.inquiry();
+    this.columnDefs = [
+      {
+        headerName: '#',
+        field: 'id', minWidth: 50, width: 50
+      },
+      {
+        headerName:'image',
+        field: 'status',
+        cellRenderer: 'srcImg',
+        cellClass: 'text-center',
+        sortable: false,
+        filter: false,
+      },
+      {
+        headerName: 'userName',
+        field: 'userName'
+      },
+      {
+        headerName: 'enable',
+        field: 'enabled',
+        cellRenderer: 'status'
+      },
+      {
+        headerName: 'fullName',
+        field: 'fullName'
+      },
+      {
+        headerName: 'dateBirth',
+        field: 'dateBirth'
+      },
+      {
+        headerName: 'gender',
+        field: 'gender'
+      },
+      {
+        headerName: 'phoneNumber',
+        field: 'phoneNumber',
+        cellRenderer: 'phoneNumber',
+      },
+      {
+        headerName: 'remark',
+        field: 'remark',
+      }
+    ];
+    this.frameworkComponents = {
+      srcImg: SrcComponent
+    };
+    this.defaultColDef = {
+      editable: false,
+      sortable: true,
+      flex: 1,
+      minWidth: 100,
+      filter: true,
+      resizable: true,
+    };
+
+    this.rowSelection = 'multiple';
+    this.isRowSelectable = function (rowNode: any) {
+      return rowNode.data;
+    };
   }
+
+  onSelectionChanged(event: any) {
+    const selectedRows = this.gridApi.getSelectedRows();
+    // this.selectedJson = selectedRows[0];
+    // console.log(this.selectedJson);
+    // if(this.selectedJson.enabled === false) {
+    //   this.btnEnableTxt = this.translate.instant('users.label.enableUser');
+    //   this.enable = true;
+    // } else {
+    //   this.btnEnableTxt = this.translate.instant('users.label.disableUser');
+    //   this.enable = false;
+    // }
+    // if(selectedRows) {
+    //   this.disabled = false;
+    // }
+  }
+
+
+  onCellDoubleClicked(event:any) {
+    if(event) {
+      // const jsonString = JSON.stringify(this.lstUser[event.rowIndex]);
+      // const encryptString = EncryptionUtil.encrypt(jsonString.toString()).toString();
+      // Utils.setSecureStorage(LOCAL_STORAGE.UserDetail, encryptString);
+      // this.router.navigate(['account/user-detail']);
+    }
+  }
+
+  onGridReady(params:any) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    // this.inquiry();
+  }
+
+  inquiry() {
+    const api = '/api/user/v0/read';
+    const data: Data[] = [];
+    for (let index = 0; index < 1000000; index++) {
+      data.push({
+        id: index,
+        status: 'status-'+index,
+        userName:'userName-'+index,
+        enabled: 'enabled-'+index,
+        fullName: 'fullName-'+index,
+        dateBirth: 'dateBirth-'+index,
+        gender: 'gender-'+index,
+        phoneNumber: 'phoneNumber-'+index,
+        remark: 'remark-'+index,
+      });
+    }
+    this.rowData = data;
+    // this.hTTPService.Get(api).then(response => {
+    //   if(response.result.responseCode !== HTTPResponseCode.Success) {
+    //     this.showErrMsg(response.result.responseMessage);
+    //  }else {
+    //     this.lstUser = response.body;
+    //     this.rowData =this.lstUser;
+    //   }
+    // });
+  }
+
+  makePdf() {
+    let doc = new jsPDF();
+    doc.setTextColor(255, 0, 0);
+    doc.text('Hello world!', 20, 20,{align: "center", baseline:'bottom'});
+    // doc.addPage();
+    doc.setDrawColor(255, 0, 0);
+    doc.line(35, 30, 100, 30);
+    doc.save('Test.pdf');
+  }
+
+}
+
+export interface Data {
+  id: number
+  status: string;
+  userName:  string;
+  enabled:  string;
+  fullName:  string;
+  dateBirth:  string;
+  gender:  string;
+  phoneNumber:  string;
+  remark:  string;
 
 }
